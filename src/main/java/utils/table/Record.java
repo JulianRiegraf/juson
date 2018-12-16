6c8f329420c6480959dac440ba3d5e664d59784e
JulianRiegraf <julianriegraf@web.de>
2018-12-16 20:45:43 +0100

    changed list to map in Record.data

--- src/main/java/utils/table/Record.java
+++ src/main/java/utils/table/Record.java
@@ -1,42 +1,63 @@
 package utils.table;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 /**
  * Represents a record (row) of a database table.
  */
 public class Record {
 
-  private List<String> data;
+  private Map<Integer, String> data;
   private Table table;
 
   public Record(Table table) {
     this.table = table;
-    data = new ArrayList<>(table.getColumns().size());
+    data = new HashMap<>(table.getColumns().size());
   }
 
   public Table getTable() {
     return table;
   }
 
-  public int size() {
-    return data.size();
+  public Record addData(int index, String data) {
+    this.data.put(index, data);
+    return this;
   }
 
-  public void addData(String data) {
-    this.data.add(data);
+  public Record addData(String columnName, String data) {
+    Optional<Column> column = this.table.getColumns().stream()
+        .filter(x -> x.name.equalsIgnoreCase(columnName)).findFirst();
+    if (column.isEmpty()) {
+      throw new NoSuchElementException(
+          "There is no column '" + columnName + "' in table  '" + table.getName() + "'");
+    }
+    int index = table.getColumns().indexOf(column.get());
+    this.data.put(index, data);
+    return this;
   }
 
-  public void addData(List<String> data) {
-    data.forEach(x -> this.data.add(x));
+  public Optional<String> getData(String columnName) {
+    Optional<Column> column = this.table.getColumns().stream()
+        .filter(x -> x.name.equalsIgnoreCase(columnName)).findFirst();
+    if (column.isEmpty()) {
+      throw new NoSuchElementException(
+          "There is no column '" + columnName + "' in table  '" + table.getName() + "'");
+    }
+    int index = table.getColumns().indexOf(column.get());
+    return Optional.ofNullable(data.get(index));
+  }
+
+  public Optional<String> getData(int index) {
+    return Optional.ofNullable(data.get(index));
   }
 
   public List<String> getData() {
-    return data;
+    return new ArrayList<>(data.values());
   }
 
-  public void setData(List<String> data) {
-    this.data = data;
-  }
 }
